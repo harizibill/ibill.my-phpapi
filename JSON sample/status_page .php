b6e33efb7e9596c16e6f1fff6eb523e1c7712987
MSAF Consolidated <37065033+ibillmy@users.noreply.github.com>
2018-04-14 20:06:19 +0800

    Add files via upload

--- JSON sample/status_page .php
+++ JSON sample/status_page .php
@@ -2,14 +2,6 @@
 
 //SAMPLE MERCHANT STATUS PAGE
 
-$std_status_code=$_POST['std_status_code']; 		//Code payment status
-$std_status=$_POST['std_status']; 					//Payment Status
-$std_order_id=$_POST['std_order_id'];				//Order ID send by your system
-$std_purchase_code=$_POST['std_purchase_code'];		//Purchase Code
-$std_secret=$_POST['std_secret'];					//Your secret key
-$std_amount=$_POST['std_amount'];					//Total Amount Customer Pay
-$std_datepaid=$_POST['std_datepaid'];				//Time Customer make payment
-
 
 // if Code payment status is 00 == payment success 
 
@@ -18,12 +10,12 @@ $std_datepaid=$_POST['std_datepaid'];				//Time Customer make payment
 		//Decode JSON
 		$objJSON = json_decode($getJSON);
 		$std_status_code = $objJSON->{'std_status_code'}; 		//Code payment status
-		$std_status = $objJSON->{'std_status'};				//Payment Status
-		$std_order_id = $objJSON->{'std_order_id'};			//Order ID send by your system
+		$std_status = $objJSON->{'std_status'};					//Payment Status
+		$std_order_id = $objJSON->{'std_order_id'};				//Order ID send by your system
 		$std_purchase_code = $objJSON->{'std_purchase_code'};	//Purchase Code
-		$std_secret = $objJSON->{'std_secret'};				//Your secret key
+		$std_secret = $objJSON->{'std_secret'};					//Your secret key
 		$std_amount = $objJSON->{'std_amount'};					//Total Amount Customer Pay
-		$std_datepaid = $objJSON->{'std_datepaid'};			//Time Customer make payment
+		$std_datepaid = $objJSON->{'std_datepaid'};				//Time Customer make payment
 		
 		
 		if($std_secret!="yoursecretkey")								//check your secret key
